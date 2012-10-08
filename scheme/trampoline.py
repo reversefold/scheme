ae98a7e90dfa576f86ca1fe0460f2bb29222addf
Justin Patrin <justin@idle-games.com>
2012-10-08 15:52:54 -0700

    Make the counting a subclass so it's easy to swap in

--- scheme/trampoline.py
+++ scheme/trampoline.py
@@ -1,24 +1,42 @@
 # Borrowed from Davy Wybiral at http://knol.google.com/k/davy-wybiral/trampolining-in-python/23oi5sywhe2tp/2#
 
-class Bounce:
-#    i = 0
-#    c = 0
-
+class Bounce(object):
     def __init__(self, func, *args, **kwargs):
-#        Bounce.i += 1
         self.func = func
         self.args = args
         self.kwargs = kwargs
         #self.__call__ = lambda: func(*args, **kwargs)
 
     def __call__(self):
-#        Bounce.c += 1
-#        print "%r %r %r" % (self.func, self.args, self.kwargs)
-#        if Bounce.c % 100000 == 0:
-#            print "Bounce %i %i" % (Bounce.i, Bounce.c)
         return self.func(*self.args, **self.kwargs)
+
+
+class CountedBounce(Bounce):
+    i = 0
+    c = 0
+
+    def __init__(self, func, *args, **kwargs):
+        CountedBounce.i += 1
+        super(CountedBounce, self).__init__(func, *args, **kwargs)
+
+    def __call__(self):
+        CountedBounce.c += 1
+#        print "%r %r %r" % (self.func, self.args, self.kwargs)
+        self.output()
+        return super(CountedBounce, self).__call__()
+
+    @classmethod
+    def output(cls, force=False):
+        if force or cls.c % 100000 == 0:
+            print "CountedBounce %i %i" % (cls.i, cls.c)
+
+
+#Bounce = CountedBounce
+
 
 def trampoline(value):
     while isinstance(value, Bounce):
         value = value()
+    if Bounce is CountedBounce:
+        CountedBounce.output(True)
     return value
