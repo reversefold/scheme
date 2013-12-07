ef99be2a87d71174d1204a78972c2885c1051f4b
papercrane <papercrane@reversefold.com>
2013-12-06 20:01:28 -0800

    use __name__

--- run.py
+++ run.py
@@ -2,11 +2,17 @@ from scheme import Scheme
 from scheme.parser import SchemeParser
 import sys
 
-expression = sys.stdin.read()
 
-print "Input:    %s" % (expression,)
-token = SchemeParser(expression).parse()
-print "Token:    %s" % (token,)
-s = Scheme(token)
-result = s.ceval()
-print "Value:    %s" % (result,)
+def main():
+    expression = sys.stdin.read()
+
+    print "Input:    %s" % (expression,)
+    token = SchemeParser(expression).parse()
+    print "Token:    %s" % (token,)
+    s = Scheme(token)
+    result = s.ceval()
+    print "Value:    %s" % (result,)
+
+
+if __name__ == '__main__':
+    main()
